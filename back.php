84d59d6aecaab2ce1fd76f33669b6d311d613b26
Alex Tran <Alex Tran>
2016-03-03 20:19:50 -0800

    Column widths for tags

--- back.php
+++ back.php
@@ -4,7 +4,7 @@
 
 .button {
   display: inline-block;
-  border-radius: 4px;
+  border-radius: 10px;
   background-color: #529ECC;
   border: none;
   color: #FFFFFF;
@@ -43,7 +43,7 @@
 
 input[type=submit] {
   display: inline-block;
-  border-radius: 4px;
+  border-radius: 10px;
   background-color: #529ECC;
   border: none;
   color: #FFFFFF;
@@ -70,7 +70,7 @@ body{
 </style>
 
 <body>
-<div style="width:450px; margin: 0 auto;">
+<div style="width:450px; margin: 0 auto; padding-bottom: 50px;">
 		<button class="button" style="vertical-align:middle"><span>TumblrDownloader</span></button>
 </div>
 
@@ -118,13 +118,18 @@ $client = new Tumblr\API\Client(
   'gmB0Z5UYW1KsRue8KbABRxTbdwwjrugCF2kSyaaO2XxCvASSaX'
 );
 
+$numPages = 25;
 $limit = 50;
 $feedURL = 'http://';
 $feedURL = $feedURL . $target_user;
+$feedURL .= '.tumblr.com/api/read/?start=';
 $query = array();
 
 if(isset($_POST['filter_query']) && $_POST['filter_query']!=""){
 	foreach($str_array as $str){
+		
+		
+		
 		$feedURL .= '.tumblr.com/api/read/?start=0&num='.$limit.'&tagged='.$str;
 		$xml = simplexml_load_file($feedURL);
 
@@ -160,8 +165,6 @@ if(isset($_POST['filter_query']) && $_POST['filter_query']!=""){
 			}
 		}
 		
-		
-		
 		echo	
 				"<table border='1' style='width:100%; background-color:#FFFFFF;'>
 				<tr>
@@ -175,17 +178,26 @@ if(isset($_POST['filter_query']) && $_POST['filter_query']!=""){
 		//Display images
 		foreach($f_arr as $arr){
 			
+			/*
+			Array{
+				Caption
+				Image
+				Width
+				Height
+				Tag
+			}
+			*/
 			echo
 			"
 			<tr>
-			<td style='text-align:center;word-wrap:break-word; max-width:200px;'>".$arr[0]."</td>";
+			<td style='text-align:center;word-wrap:break-word; width:800px;'>".$arr[0]."</td>";
 			echo "
-			<td style='text-align:center;'>
-				<a href='".$arr[1]."'download='myimage'><img style='width:50px;' src='".$arr[1]."'/></a>
+			<td style='text-align:center; width:150'>															
+				<a href='".$arr[1]."'download='myimage'><img style='width:200px;' src='".$arr[1]."'/></a>
 			</td>
-			<td style='text-align:center;'>".$arr[2]."</td>
-			<td style='text-align:center;'>".$arr[3]."</td>
-			<td style='text-align:center;'>".$arr[4]."</td>
+			<td style='text-align:center; width: 100px;'>".$arr[2]."</td>
+			<td style='text-align:center; width: 100px;'>".$arr[3]."</td>
+			<td style='text-align:center; width: 100px;'>".$arr[4]."</td>
 			</tr>
 			";
 			
@@ -194,12 +206,27 @@ if(isset($_POST['filter_query']) && $_POST['filter_query']!=""){
 	}
 }
 else{
-
-	$feedURL .= '.tumblr.com/api/read/?start=0&num='.$limit;
-	$xml = simplexml_load_file($feedURL);
-	$j = 0;
-	$query = array();
 	
+	echo	
+		"<table border='1' style='width:100%; background-color:#FFFFFF'>
+		<tr>
+			<th>Caption</th>
+			<th>Image</th>
+			<th>Width</th>
+			<th>Height</th>
+		</tr>
+		";
+				
+	
+	
+	if(isset($_GET['total'])){ 
+		$count = $_GET['total'];
+	}  
+	$j = 0;
+	$URL = $feedURL.$j.'&num='.$limit;
+	$xml = simplexml_load_file($URL);
+	$query = array();
+
 	//Store images into array
 	foreach($xml->posts->post as $post){
 		if(strlen((string)$post->{'photo-url'})>0){
@@ -211,9 +238,9 @@ else{
 		   array_push($query,$post_arr);
 	   }
 	}
-	
+
 	$f_arr = array();
-	
+
 	foreach($query as $filter){
 
 		if($width_query!=null && $filter[2]<$width_query && $height_query!=null && $filter[3]<$height_query){
@@ -230,21 +257,17 @@ else{
 			array_push($f_arr,$filter);
 		}
 	}
-	
-	
-	
-	echo	
-			"<table border='1' style='width:100%; background-color:#FFFFFF'>
-			<tr>
-				<th>Caption</th>
-				<th>Image</th>
-				<th>Width</th>
-				<th>Height</th>
-			</tr>
-			";
+
 	//Display images
 	foreach($f_arr as $arr){
-		
+		/*
+			Array{
+				Caption
+				Image
+				Width
+				Height
+			}
+			*/
 		echo
 		"
 		<tr>
